9d4dc0bd5be589f6707d28d1e719715c696da707
kwang <kwanggod00@gamil.com>
2025-11-17 16:12:32 +0900

    feat: 맵풀링 작업 스크립트 생성

---
+++ Assets/Scripts/Map/MapPooling.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPooling : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] mapPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
