007bcfe9b63196f90db469adf371d9244d918ba1
KimJongWeon <dhdlqns12@gmail.com>
2025-11-17 15:22:20 +0900

    feat: 장애물(초기,충돌 X)

---
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour, IObstacle
+{ 
+    public void Init()
+    {
+
+    }
+
+    public void Interact(GameObject player)
+    {
+        // 플레이어와 충돌 시
+
+    }
+}
