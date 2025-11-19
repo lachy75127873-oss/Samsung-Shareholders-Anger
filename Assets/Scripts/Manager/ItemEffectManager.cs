50740f5ff2733d2d35a58721385d3e5d3729daaa
KimJongWeon <dhdlqns12@gmail.com>
2025-11-19 10:28:13 +0900

    fix: 연동 문제와 버프 만료처리 추가

--- Assets/Scripts/Manager/ItemEffectManager.cs
+++ Assets/Scripts/Manager/ItemEffectManager.cs
@@ -7,6 +7,20 @@ public class ItemEffectManager
     private List<ActiveItem> activeItems = new List<ActiveItem>();
     private int totalCoins = 0; //scoreManger 또는 GameManger에서 관리할거 임시용
 
+    private PlayerController playerController;
+
+    // 원본 스탯 저장
+    private float originalJumpPower;
+    private float originalMaxHeight;
+
+    public int GetTotalCoins() => totalCoins;
+    public List<ActiveItem> GetActiveItems() => activeItems;
+
+    public ItemEffectManager(PlayerController player)
+    {
+        playerController = player;
+    }
+
     public void Update()
     {
         for (int i = activeItems.Count - 1; i >= 0; i--)
@@ -15,10 +29,19 @@ public class ItemEffectManager
 
             if (activeItems[i].remainingTime <= 0)
             {
+                // 버프 만료 처리
+                OnBuffExpired(activeItems[i]);
+
                 Debug.Log($"{activeItems[i].itemData.itemName} 효과 종료");
                 activeItems.RemoveAt(i);
             }
         }
+    }
+
+    public void SaveOriginalStats(float jumpPower, float maxHeight)
+    {
+        originalJumpPower = jumpPower;
+        originalMaxHeight = maxHeight;
     }
 
     public void ApplyItem(ItemData itemData)
@@ -43,13 +66,16 @@ public class ItemEffectManager
 
     private void ApplyCoin(ItemData itemData)
     {
-        totalCoins += 1;
-        Debug.Log($"코인 획득: +{1} (총: {totalCoins})");
+        int coinValue = GetValue(itemData, 0, 1);
+        totalCoins += coinValue;
+        Debug.Log($"코인 획득: +{coinValue} (총: {totalCoins})");
     }
 
     private void ApplyBuff(ItemData itemData)
     {
         ActiveItem existing = activeItems.Find(x => x.itemData.itemType == itemData.itemType);
+
+        bool isNewBuff = false;
 
         if (existing != null)
         {
@@ -65,9 +91,67 @@ public class ItemEffectManager
                 remainingTime = itemData.duration,
                 isActive = true
             };
-
             activeItems.Add(buff);
+            isNewBuff = true;
             Debug.Log($"{itemData.itemName} 활성화 ({itemData.duration}초)");
         }
+
+        switch (itemData.itemType)
+        {
+            case ItemData.ItemType.JumpUp:
+                ApplyJumpStats(itemData);
+                break;
+
+            case ItemData.ItemType.Magnet:
+                if (isNewBuff)
+                {
+                    float magnetRange = GetValue(itemData, 0, 5);
+                    playerController.EnableMagnetRange(magnetRange);
+                }
+                break;
+        }
+    }
+
+    private void OnBuffExpired(ActiveItem buff)
+    {
+        if (buff.itemData.itemType == ItemData.ItemType.JumpUp)
+        {
+            RestoreJumpStats();
+        }
+        else if (buff.itemData.itemType == ItemData.ItemType.Magnet)
+        {
+            playerController.DisableMagnetRange();
+        }
+    }
+
+    private void ApplyJumpStats(ItemData itemData)
+    {
+        float jumpPowerBonus = GetValue(itemData, 0, 0);
+        float maxHeightBonus = GetValue(itemData, 1, 0);
+
+        playerController.SetJumpPower(originalJumpPower + jumpPowerBonus);
+        playerController.SetMaxHeight(originalMaxHeight + maxHeightBonus);
+    }
+
+    private void RestoreJumpStats()
+    {
+        playerController.SetJumpPower(originalJumpPower);
+        playerController.SetMaxHeight(originalMaxHeight);
+    }
+
+    /// <summary>
+    /// Json의 value배열에서 값 가져오기
+    /// </summary>
+    /// <param name="itemData"></param>
+    /// <param name="index"></param>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    private int GetValue(ItemData itemData, int index, int defaultValue = 0)
+    {
+        if (itemData.value == null || itemData.value.Length <= index)
+        {
+            return defaultValue;
+        }
+        return itemData.value[index];
     }
 }
