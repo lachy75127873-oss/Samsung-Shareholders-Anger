d0e8da39dfb8fcb57407419d166dc4857b197c99
Park ha <115542242+parkha6@users.noreply.github.com>
2025-11-19 16:15:37 +0900

    fix 메인화면 수정

--- Assets/Scripts/UI/StartMenuScene.cs
+++ Assets/Scripts/UI/StartMenuScene.cs
@@ -31,12 +31,30 @@ public class StartMenuScene : MonoBehaviour
     [Tooltip("옵션 나가기 버튼")]
     [SerializeField] Button optionExitButton;
     /// <summary>
+    /// 마지막 스코어
+    /// </summary>
+    [Tooltip("마지막 스코어")]
+    [SerializeField] Text lastScore;
+    /// <summary>
+    /// 최고점수
+    /// </summary>
+    [Tooltip("최고점수")]
+    [SerializeField] Text highestScore;
+    /// <summary>
+    /// 전판 점수 대비 퍼센트
+    /// </summary>
+    [Tooltip("전판 점수 대비 퍼센트")]
+    [SerializeField] Text percentScore;
+    /// <summary>
     /// 로딩씬 스크립트 넣는 곳
     /// </summary>
     [SerializeField]
-    LoadingScene loadingScene;//싱글턴으로 로딩씬 처리
+    LoadingScene loadingScene;//싱글턴으로 로딩씬 처리해야 됨
     private void Awake()
     {
+        lastScore.text = 0.ToString();
+        highestScore.text = 0.ToString();
+        percentScore.text = $"{0.ToString()}%";//임시임. 스코어 매니저랑 상의해야 됨.
         startButton.onClick.AddListener(StartGame);
         optionButton.onClick.AddListener(TurnOption);
         optionExitButton.onClick.AddListener(ExitOption);
